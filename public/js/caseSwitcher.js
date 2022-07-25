e7af0f5cb205bc099487f000f0b2d06f0ecc0f91
Jack Whale <jemwhale@gmail.com>
2022-07-25 14:35:00 +0100

    case switcher commit

---
+++ public/js/caseSwitcher.js
@@ -0,0 +1,27 @@
+const p = document.getElementById('patrick');
+const quietBtn = document.getElementById('quiet-btn');
+const loudBtn = document.getElementById('loud-btn');
+const sarcBtn = document.getElementById('sarc-btn');
+quietBtn.addEventListener('click', lowerCase);
+loudBtn.addEventListener('click', upperCase);
+sarcBtn.addEventListener('click', sarcCase);
+function lowerCase(e){
+    p.innerText = p.innerText.toUpperCase()
+}
+function upperCase(e){
+    p.innerText = p.innerText.toLowerCase()
+}
+function sarcCase(e){
+    let low = p.innerText.toLowerCase();
+    let high = p.innerText.toUpperCase();
+    let newStr = ''
+    for (i=0; i<low.length; i++){
+        if(i%2 === 0){
+            newStr += high[i]
+        }else{
+            newStr += low[i]
+        }
+    }
+    p.innerText = newStr
+}
+
