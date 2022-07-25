1e8cfc61387a661670d9c201f1000d45e26c085b
Jack Whale <jemwhale@gmail.com>
2022-07-25 16:02:56 +0100

    case switcher fix again

--- public/js/caseSwitcher.js
+++ public/js/caseSwitcher.js
@@ -11,16 +11,37 @@ function lowerCase(e){
 function upperCase(e){
     p.innerText = p.innerText.toUpperCase()
 }
+// function sarcCase(e){
+//     let low = p.innerText.toLowerCase();
+//     let high = p.innerText.toUpperCase();
+//     let newStr = ''
+//     for (i=0; i<low.length; i++){
+        
+//         if(i%2 !== 0){
+//             newStr += high[i]
+//         }else{
+//             newStr += low[i]
+//         }
+//     }
+//     p.innerText = newStr
+// }
+
 function sarcCase(e){
-    let low = p.innerText.toLowerCase();
-    let high = p.innerText.toUpperCase();
-    let newStr = ''
-    for (i=0; i<low.length; i++){
-        if(i%2 !== 0){
-            newStr += high[i]
+let count = 0
+let newStr = ''
+    for (i in p.innerText){
+        
+        if (/^[0-9a-zA-Z]+$/.test(p.innerText[i]) == false){
+            newStr += p.innerText[i]
         }else{
-            newStr += low[i]
+
+        if(count%2 === 0){
+            newStr += p.innerText[i].toLowerCase()
+        }else{
+            newStr += p.innerText[i].toUpperCase()
         }
+        count ++
+    }
     }
     p.innerText = newStr
 }
